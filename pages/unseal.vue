17f8dad8e5b55cf406e526512d2595a2afc66f9d
yuanli <2865437316@qq.com>
2023-05-15 18:09:22 +0800

    feat: 其他页面ui

--- pages/unseal.vue
+++ pages/unseal.vue
@@ -1,13 +1,24 @@
 <template>
-    <div class="text-white">
-      Unseal
-</div>
+  <div class="container px-8 mx-auto mt-12">
+    <div class="max-w-2xl mx-auto space-y-8">
+      <Title text="解密文档"/>
+      <n-input type="text" size="large" placeholder="请输入加密的key"/>
+      <n-button
+          type="primary"
+          text-color="#fff"
+          size="large"
+          icon-placement="right"
+          class="w-full"
+      >
+        解密
+      </n-button>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "unseal"
-}
+<script setup>
+import Title from '../components/title'
+
 </script>
 
 <style scoped>
